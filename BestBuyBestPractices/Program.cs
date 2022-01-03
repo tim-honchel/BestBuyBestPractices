b3d6489367d3032c019983e29cf0cbc82c2e8932
Tim Honchel <tim.honchel@gmail.com>
2022-01-03 16:03:59 -0500

    try number 2

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -2,11 +2,12 @@
 
 namespace BestBuyBestPractices
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int test = 0;
         }
     }
 }
